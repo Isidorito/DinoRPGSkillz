bc0748dc141dab9cdb1970d9e5e7b94aabc90d98
Isidorito <isidorito@outlook.com>
2015-07-03 17:57:30 -0300

    Version 0.0.1 - Not finished

---
+++ app/src/main/java/com/isidorito/dinorpgskills/Splash.java
@@ -0,0 +1,17 @@
+package com.isidorito.dinorpgskills;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Isidorito on 7/3/2015.
+ */
+public class Splash extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.Splash);
+    }
+}
